c734d8fe09e826c1d5c3067b20a88a07ab1f6e4f
simranjeet Singh <simranb7896@gmail.com>
2020-10-16 02:28:38 +0530

    Revert "km"
    This reverts commit ff0648a562172cfdd1cf0acccc2496706c14484b.

--- Assignment_2/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace Assignment_2.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
--- Assignment_2/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace Assignment_2.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
